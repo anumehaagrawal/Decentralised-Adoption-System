51a9926c54a9ed262f8ea8ceefba6baa0dfbc748
anumehaagrawal <anuzenith29@gmail.com>
2018-12-14 23:04:33 +0530

    Voting application added

---
+++ Voting/contracts/Election.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.24;
+contract Election{
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    mapping(uint=>Candidate) public candidates;
+    mapping(address => bool) public voters;
+    uint public candidatesCount;
+    function Election() public{
+        addCandidate("Candidate_1");
+        addCandidate("Candidate_2");
+    }
+    function addCandidate(string _name) private{
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount,_name,0);
+    }
+    function vote(uint _candidateId) public{
+        require(!voters[msg.sender]);
+        require(_candidateId > 0 && _candidateId<=candidatesCount);
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount ++;
+    }
+    
+}
---
+++ Voting/src/js/app.js
@@ -0,0 +1,110 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  account: '0x0',
+
+  init: function() {
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      const defaultAccount = web3.eth.defaultAccount
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
+    return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON("Election.json", function(election) {
+      // Instantiate a new truffle contract from the artifact
+      App.contracts.Election = TruffleContract(election);
+      // Connect provider to interact with contract
+      App.contracts.Election.setProvider(App.web3Provider);
+
+      return App.render();
+    });
+  },
+
+  render:function() {
+    var electionInstance;
+    var loader = $("#loader");
+    var content = $("#content");
+
+    loader.show();
+    content.hide();
+    web3.eth.getAccounts((err, res) => {                   
+      console.log(res[0]);
+    });
+
+    web3.eth.getAccounts(function(err, accounts) {
+  
+      if (err === null && accounts != null) {
+        App.account = accounts[0];
+      
+        $("#accountAddress").html("Your Account: " + accounts[0]);
+      }
+    });
+
+    // Load contract data
+    App.contracts.Election.deployed().then(function(instance) {
+      electionInstance = instance;
+      return electionInstance.candidatesCount();
+    }).then(function(candidatesCount) {
+      var candidatesResults = $("#candidatesResults");
+      candidatesResults.empty();
+
+      var candidateSelect = $("#candidatesSelect");
+      candidateSelect.empty();
+
+      for (var i = 1; i <= candidatesCount; i++) {
+        electionInstance.candidates(i).then(function(candidate) {
+          var id = candidate[0];
+          var name = candidate[1];
+          var voteCount = candidate[2];
+
+          // Render candidate Result
+          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+          candidatesResults.append(candidateTemplate);
+
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+          candidateSelect.append(candidateOption);
+        });
+      }
+      return electionInstance.voters(App.account);
+    }).then(function(hasVoted){
+      if(hasVoted){
+        $('form').hide();
+      }
+      loader.hide();
+      content.show();
+    }).catch(function(error) {
+      console.warn(error);
+    });
+},
+
+castVote:function(){
+  var candId = $("#candidatesSelect").val();
+   App.contracts.Election.deployed().then(function(instance){
+      return instance.vote(candId,{from:App.account});
+  }).then(function(result){
+    $("#content").hide();
+    $("#loader").show();
+  }).catch(function(error){
+      console.warn(error);
+  });
+}
+};
+
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
